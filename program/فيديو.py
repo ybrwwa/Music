39f534dbe79ba7851455c4f246f5a9353912770a
ybrwwa <105541436+ybrwwa@users.noreply.github.com>
2022-05-28 07:48:47 +0200

    Update فيديو.py

--- program/فيديو.py
+++ program/فيديو.py
@@ -56,7 +56,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command2(["ف تشغيل","فيديو تشغيل"]) & other_filters)
+@Client.on_message(command2(["فيديو","فيديو تشغيل"]) & other_filters)
 async def vplay(c: Client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
